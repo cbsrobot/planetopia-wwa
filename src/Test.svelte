2059dbfd47ebfd00a8f04f30b3c364c802c6fd47
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-09-13 18:52:30 +0200

    add blank main component for testing single components

---
+++ src/Test.svelte
@@ -0,0 +1,30 @@
+<script>
+  import ProgressIndicator from "./ProgressIndicator.svelte";
+  import Button from "./Button.svelte";
+  import { _ , setLocale } from "./i18n.js";
+</script>
+
+<main>
+  <Button home on:click={() => setLocale("en")} />
+  <ProgressIndicator position={5} length={9}/>
+</main>
+
+<style>
+  main {
+    position: absolute;
+    display: block;
+    left: 0;
+    top: 0;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 1920px;
+    height: 1080px;
+    overflow: hidden;
+    margin: 0 auto;
+    font-family: "Montserrat", sans-serif;
+    font-size: 2rem;
+    background-color: #f2f2f2;
+  }
+</style>
