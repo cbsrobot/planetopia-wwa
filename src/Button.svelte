12574a7eca883e37b6b5b71f40aa04713a13faa2
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-09-13 18:46:01 +0200

    button: add i18n

--- src/Button.svelte
+++ src/Button.svelte
@@ -1,31 +1,33 @@
 <script>
-  const STANDARD_TEXT = "Click me!"
+  import { _ } from "./i18n.js";
+
   // choose from 5 different variants:
   // with icons: next, back, home
   // without icons: plain-primary, plain-secondary
   export let next = false, back = false, home = false, plain_primary = false, plain_secondary = false;
-  export let text = STANDARD_TEXT;
 
-  // TODO: Add language localization
-  $: if (text === STANDARD_TEXT) {
-		if(back) text = "Zurück"
-    if(next) text = "Weiter"
-    if(home) text = "Zum Anfang"
-	}
-
+  export let text = ""
   export let disabled = false;
+
+  let defaultText = "Click me!"
+  $:{
+    if (back) defaultText = $_("back");
+    if (next) defaultText = $_("next");
+    if (home) defaultText = $_("home");
+  }
+  $: buttonText = text != "" ? text : defaultText;
 </script>
 
-<button class:disabled class:next class:back class:home class:plain_primary class:plain_secondary>
+<button on:click class:disabled class:next class:back class:home class:plain_primary class:plain_secondary>
   {#if back}
-    <img class="icon" alt="" src="assets/icons/back.svg">
+    <img class="icon" alt="" src="assets/icons/back.svg" />
   {/if}
-  <span>{text}</span>
+  <span>{buttonText}</span>
   {#if next}
-    <img class="icon" alt="" src="assets/icons/next.svg">
+    <img class="icon" alt="" src="assets/icons/next.svg" />
   {/if}
   {#if home}
-    <img class="icon" alt="" src="assets/icons/home.svg">
+    <img class="icon" alt="" src="assets/icons/home.svg" />
   {/if}
 </button>
 
@@ -43,11 +45,11 @@
     align-self: center;
     padding: 1rem 1rem;
     margin: 0 1rem;
-    color: #2D2D2D;
+    color: #2d2d2d;
     font-size: 2rem;
     outline: none;
 
-    border:solid 5px;
+    border: solid 5px;
 
     border-top-left-radius: 255px 15px;
     border-top-right-radius: 15px 225px;
@@ -55,26 +57,29 @@
     border-bottom-left-radius: 15px 255px;
   }
 
-  .plain_primary, .next{
-    background-color: #2D2D2D;
+  .plain_primary,
+  .next {
+    background-color: #2d2d2d;
     color: white;
     border-color: white;
   }
 
-  .plain_primary:active, .next:active{
+  .plain_primary:active,
+  .next:active {
     background-color: hsla(0, 0%, 18%, 0.6);
   }
 
-  .plain_secondary, .back, .home{
-    border-color: #2D2D2D;
+  .plain_secondary,
+  .back,
+  .home {
+    border-color: #2d2d2d;
   }
 
-  span{
+  span {
     padding: 0 0.5rem;
   }
 
-  img{
+  img {
     padding: 0 0.3rem;
   }
-
 </style>
