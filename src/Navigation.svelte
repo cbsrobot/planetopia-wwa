f15d3e4a640cc55b2f5de2c215a257990f40a9ff
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-09-14 10:35:04 +0200

    add navigation component

---
+++ src/Navigation.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Button from "./Button.svelte";
+</script>
+
+<div class="container top">
+  <Button back />
+  <Button home />
+</div>
+
+<div class="container bottom">
+  <Button next />
+</div>
+
+<style>
+  .container {
+    width: 95%;
+    display: flex;
+  }
+
+  .top{
+    justify-content: space-between;
+    
+    position: absolute;
+    top: 5%;
+  }
+
+  .bottom{
+    justify-content:flex-end;
+  
+    position: absolute;
+    bottom: 5%;
+  }
+</style>
