6d48ea56deadade10852e7a063235f0f0dd8db8d
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-09-15 12:09:28 +0200

    add dialogue page component

---
+++ src/components/DialoguePage.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Navigation from "./Navigation.svelte";
+  //   import { userData } from "../modules/DataManager";
+  let userData = { avatar: 3 };
+
+  export let dialogueText = "Listen to my dialogue";
+  export let pageIndex;
+</script>
+
+
+<Navigation on:nextClick={() => pageIndex++} on:backClick={() => pageIndex--} />
+<img alt="Insect Avatar" src="assets/avatar/{userData.avatar}.jpg" />
+<p id="dialogue">{dialogueText}</p>
+
+
+<style>
+  img {
+    position: absolute;
+    top: 250px;
+    left: 0;
+    width: 800px;
+  }
+
+  #dialogue {
+    display: block;
+    position: absolute;
+    text-align: left;
+    font-size: 3rem;
+
+    top: 380px;
+    left: 850px;
+    width: 900px;
+    height: 360px;
+  }
+</style>
--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,14 +1,18 @@
 <script>
   import Button from "./Button.svelte";
+  import { logOut } from "../modules/SessionManager.js";
+
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
 </script>
 
 <div class="container top">
-  <Button back />
-  <Button home />
+  <Button back on:click={() => dispatch("backClick")} />
+  <Button home on:click={() => logOut()} />
 </div>
 
 <div class="container bottom">
-  <Button next />
+  <Button next on:click={() => dispatch("nextClick")} />
 </div>
 
 <style>
@@ -17,16 +21,16 @@
     display: flex;
   }
 
-  .top{
+  .top {
     justify-content: space-between;
 
     position: absolute;
     top: 5%;
   }
 
-  .bottom{
-    justify-content:flex-end;
-  
+  .bottom {
+    justify-content: flex-end;
+
     position: absolute;
     bottom: 5%;
   }
