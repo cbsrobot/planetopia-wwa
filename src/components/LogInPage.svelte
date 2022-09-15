7b7f4ae1fb2d000495cc8d0f3f554160331bc8b9
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-09-15 17:24:14 +0200

    add LogInPage component

---
+++ src/components/LogInPage.svelte
@@ -0,0 +1,11 @@
+<div>
+  <p>Halte deinen Jeton auf den roten Punkt.</p>
+  <p>Place ton jeton sur le point rouge.</p>
+  <p>Hold your token on the red dot.</p>
+</div>
+
+<style>
+  p {
+    width: 1920px;
+  }
+</style>
