22007c513627bd6c3d1cda6367dc01cdc605a55d
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-09-15 17:22:59 +0200

    add dev bar component

---
+++ src/components/DevBar.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { rfid, loggedIn, simulateLogIn, logOut } from "../modules/SessionManager.js";
+  
+  export let text = ""
+
+  $: loggedInFlag = $loggedIn ? "🟢" : "🔴";
+</script>
+
+<div>
+  <span on:click={() => simulateLogIn()}>Log in</span> | 
+  <span on:click={() => logOut()}>Log out</span> |  
+  {loggedInFlag} {" RFID: " + $rfid} | {text}
+</div>
+
+<style>
+  div {
+    position: absolute;
+    width: 95%;
+    bottom: 15px;
+    left: 30px;
+    font-size: 1.3rem;
+    text-align: start;
+  }
+
+  span {
+    color: blue;
+    cursor: pointer;
+  }
+</style>
