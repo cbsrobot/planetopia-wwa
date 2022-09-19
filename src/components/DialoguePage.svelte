a0f762e47f95a47b8b4a8a0bf806df1758cb30e3
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-09-19 15:36:16 +0200

    add avatar info injection

--- src/components/DialoguePage.svelte
+++ src/components/DialoguePage.svelte
@@ -1,22 +1,28 @@
 <script>
   import Navigation from "./Navigation.svelte";
   import { _ } from "../modules/i18n.js";
-  
+
   //   import { userData } from "../modules/DataManager";
-  let userData = { avatar: 3 };
+  let userData = { avatar: 10 };
 
   export let textPath;
   export let pageIndex, totalPages;
 
-  $: dialogueText = $_(textPath)
+  $: dialogueText = injectAvatarInfo($_(textPath));
 
+  function injectAvatarInfo(str) {
+    // get avatar hints
+    const re = /#(\d)/i;
+    const match = str.match(re);
+    const avatarNr = userData.avatar;
+    let text = match ? str.replace(re, $_(`${match[1]}.insect${avatarNr}`)) : str;
+    return text;
+  }
 </script>
 
-
-<Navigation bind:pageIndex={pageIndex} {totalPages} />
+<Navigation bind:pageIndex {totalPages} />
 <img alt="Insect Avatar" src="assets/avatar/{userData.avatar}.jpg" />
 <p id="dialogue">{dialogueText}</p>
-
 
 <style>
   img {
