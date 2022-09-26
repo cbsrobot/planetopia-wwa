9fb3930bb7a0910202caa321cfa48c36b8576456
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-09-26 12:41:12 +0200

    add StationIndicator component

---
+++ src/components/StationIndicator.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { _ } from "../modules/i18n.js";
+  export let station = 0;
+</script>
+
+<div>
+  <img class="icon" alt="" src={`assets/icons/${station}.svg`} />
+  <span>{$_(String(station))}</span>
+</div>
+
+<style>
+  div {
+    display: block;
+    align-self: center;
+    padding: 1rem 1rem;
+    margin: 0 1rem;
+    color: #2d2d2d;
+    font-size: 2rem;
+    outline: none;
+
+    font-family: 'MANIC', sans-serif;
+    text-transform: uppercase;
+
+    position: absolute;
+    width: 95%;
+    top: 40px;
+    text-align: center;
+    pointer-events: none;
+  }
+
+  span {
+    padding: 0 0.5rem;
+  }
+
+  img {
+    padding: 0;
+  }
+</style>
