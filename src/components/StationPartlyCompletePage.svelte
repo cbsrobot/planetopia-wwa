c7b979e6f99577df95f5aeacb0bef963c9e3cc23
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-09-29 16:15:17 +0200

    update StationPartlyCompletePage

--- src/components/StationPartlyCompletePage.svelte
+++ src/components/StationPartlyCompletePage.svelte
@@ -1,10 +1,34 @@
 <script>
   import { _ } from "../modules/i18n.js";
+  import Button from "./Button.svelte";
+
+  export let pageIndex;
+
+  function handleRestartClick() {
+    if (pageIndex != undefined) pageIndex += 1;
+  }
+
+  function handleContinueClick() {}
 </script>
 
-<h2>{$_("station-partly-complete")}</h2>
+<div class="content">
+  <h2>{$_("station-partly-complete")}</h2>
+  <div class="button-container">
+    <Button text={$_("station-partly-complete.restart")} on:click={handleRestartClick} />
+    <Button text={$_("station-partly-complete.continue")} on:click={handleContinueClick} disabled={true} />
+  </div>
+</div>
 
 <style>
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .button-container{
+    display: flex;
+  }
   h2 {
     font-size: 56px;
     width: 50%;
