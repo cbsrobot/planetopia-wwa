4108962726c8c107ca2de7b2d76ce9ebec91a24c
Jean First <jeanfirst@gmail.com>
2022-10-08 14:00:19 +0200

    main.js: add createPdf() and sendEmail()

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const { app, BrowserWindow, dialog, ipcMain } = require("electron");
 const rpath = require("path");
 const serve = require("electron-serve");
 const loadURL = serve({ directory: "public" });
+const nodemailer = require("nodemailer");
+const pdf = require("pdf-creator-node");
+const fs = require("fs");
 const { SerialPort, ReadlineParser } = require("serialport");
 require("dotenv").config();
 
@@ -10,42 +13,42 @@ let serport;
 let parser;
 const baudRate = 9600;
 let allowedPorts = [
-    "/dev/tty.usbmodem143101", 
-    "/dev/tty.usbmodem143201",
-    "/dev/tty.usbmodem144101", 
-    "COM1",
-    "COM2", 
-    "COM3",
-    "/dev/ttyACM0",
-    "/dev/ttyACM1"
+  "/dev/tty.usbmodem143101",
+  "/dev/tty.usbmodem143201",
+  "/dev/tty.usbmodem144101",
+  "COM1",
+  "COM2",
+  "COM3",
+  "/dev/ttyACM0",
+  "/dev/ttyACM1"
 ];
 
 async function listSerialPorts() {
-    if (serport) return;
-    await SerialPort.list().then((ports, err) => {
-        if (err) return;
-        ports.some((p) => {
-            if (allowedPorts.includes(p.path)) {
-                let path = p.path;
-                serport = new SerialPort({ path, baudRate });
+  if (serport) return;
+  await SerialPort.list().then((ports, err) => {
+    if (err) return;
+    ports.some((p) => {
+      if (allowedPorts.includes(p.path)) {
+        let path = p.path;
+        serport = new SerialPort({ path, baudRate });
 
-                serport.on("error", function (err) {
+        serport.on("error", function (err) {
           console.log("SerialPort Error", err);
-                    serport = undefined;
-                    parser = undefined;
-                });
-
-                parser = serport.pipe(new ReadlineParser());
-                parser.on("data", (data) => {
-          mainWindow.webContents.send("rfid", data);
-                });
-            }
+          serport = undefined;
+          parser = undefined;
         });
+
+        parser = serport.pipe(new ReadlineParser());
+        parser.on("data", (data) => {
+          mainWindow.webContents.send("rfid", data);
+        });
+      }
     });
+  });
 }
 
 setInterval(() => {
-    listSerialPorts();
+  listSerialPorts();
 }, 5000);
 
 //const { SerialPort, ReadlineParser } = require('serialport')
@@ -68,91 +71,97 @@ setInterval(() => {
 let mainWindow;
 
 function isDev() {
-    return !app.isPackaged;
+  return !app.isPackaged;
 }
 
-function createWindow() {    
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
       preload: rpath.join(__dirname, "preload.js"),
-            enableRemoteModule: true,
+      enableRemoteModule: true,
       contextIsolation: false,
-        },
+    },
     icon: rpath.join(__dirname, "public/favicon.png"),
-        show: false,
-        autoHideMenuBar: true,
-    });
+    show: false,
+    autoHideMenuBar: true,
+  });
 
-    /*
-    // receive data from renderer
-    ipcMain.on('set-title', (event, title) => {
-        const webContents = event.sender
-        const win = BrowserWindow.fromWebContents(webContents)
-        win.setTitle(title)
-    })
-    */
-    
-    // send data to renderer
-    //parser.on('data', data => {
-    //  mainWindow.webContents.send('rfid', data);
-    //})
+  /*
+      // receive data from renderer
+      ipcMain.on('set-title', (event, title) => {
+          const webContents = event.sender
+          const win = BrowserWindow.fromWebContents(webContents)
+          win.setTitle(title)
+      })
+      */
 
-    //mainWindow.webContents.openDevTools()
+  // send data to renderer
+  //parser.on('data', data => {
+  //  mainWindow.webContents.send('rfid', data);
+  //})
+
+  ipcMain.on("sendEmail", (event, mailOptions) => {
+    createPdf(mailOptions);
+    sendEmail(mailOptions);
+    event.returnValue = "email_was_sent"
+  });
+
+  //mainWindow.webContents.openDevTools()
   mainWindow.webContents.on("before-input-event", (event, input) => {
-        /*
-        if (input.control && input.key.toLowerCase() === 'i') {
-            console.log('Pressed Control+I')
-            event.preventDefault()
-        }
-        */
+    /*
+    if (input.control && input.key.toLowerCase() === 'i') {
+        console.log('Pressed Control+I')
+        event.preventDefault()
+    }
+    */
 
     if (input.key.toLowerCase() === "0") {
       mainWindow.webContents.openDevTools();
       event.preventDefault();
-        }
+    }
 
     if (input.key.toLowerCase() === "9") {
       mainWindow.setKiosk(!mainWindow.kiosk);
       event.preventDefault();
-        }
+    }
 
     if (input.key === "Escape") {
       app.quit();
-        }
+    }
   });
 
-    // This block of code is intended for development purpose only.
-    // Delete this entire block of code when you are ready to package the application.
-    if (isDev()) {
+  // This block of code is intended for development purpose only.
+  // Delete this entire block of code when you are ready to package the application.
+  if (isDev()) {
     mainWindow.loadURL("http://localhost:5000/");
-    } else {
-        loadURL(mainWindow);
-    }
-    
-    // Uncomment the following line of code when app is ready to be packaged.
-    // loadURL(mainWindow);
+  } else {
+    loadURL(mainWindow);
+  }
 
-    // Open the DevTools and also disable Electron Security Warning.
-    // process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true;
-    // mainWindow.webContents.openDevTools();
+  // Uncomment the following line of code when app is ready to be packaged.
+  // loadURL(mainWindow);
 
-    // Emitted when the window is closed.
+  // Open the DevTools and also disable Electron Security Warning.
+  // process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true;
+  // mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
   mainWindow.on("closed", function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
     mainWindow = null;
-    });
+  });
 
-    // Emitted when the window is ready to be shown
-    // This helps in showing the window gracefully.
+  // Emitted when the window is ready to be shown
+  // This helps in showing the window gracefully.
   mainWindow.once("ready-to-show", () => {
     mainWindow.show();
-    });
+  });
 }
 
 // This method will be called when Electron has finished
@@ -162,30 +171,30 @@ function createWindow() {
 
 app.whenReady().then(() => {
   createWindow();
-  
+
   app.on("activate", function () {
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
-    
-    function enterKiosk() {
-      mainWindow.setKiosk(!mainWindow.kiosk);
-    }
-    
+
+  function enterKiosk() {
+    mainWindow.setKiosk(!mainWindow.kiosk);
+  }
+
   if (process.env.IS_PROD === "true") setTimeout(enterKiosk, 500);
 });
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function () {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") app.quit();
 });
 
 app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow();
 });
 // In this file you can include the rest of your app's specific main process
@@ -202,3 +211,75 @@ ipcMain.on('ping-good', event => {
 })
 */
 
+function createPdf(mailOptions){
+  // Read HTML Template
+  const html = fs.readFileSync(__dirname + "public/templates/template.html", "utf8");
+  const avatar = fs.readFileSync(__dirname + "/public/assets/avatar/1.jpg");
+  const logo = avatar.toString('base64');
+
+  let options = {
+    format: "A4",
+    orientation: "portrait",
+    border: "10mm",
+    /*
+    header: {
+      height: "45mm",
+      contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
+    },
+    footer: {
+      height: "28mm",
+      contents: {
+          first: 'Cover page',
+          2: 'Second page', // Any page number is working. 1-based index
+          default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+          last: 'Last Page'
+      }
+    }
+    */
+  };
+
+  let document = {
+    html: html,
+    data: {
+      logo: logo
+    },
+    path: "/tmp/wwa.pdf",
+    type: "",
+  };
+
+  pdf.create(document, options)
+    .then((res) => {
+      console.debug(res);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function sendEmail(mailOptions) {
+  var transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  if (process.env.EMAIL_TO !== "") {
+    // override user inpout with env setting
+    mailOptions.to = process.env.EMAIL_TO
+  }
+  
+  mailOptions.attachments = [{   // file on disk as an attachment
+    filename: 'Planetopia.pdf',
+    path: '/tmp/wwa.pdf'
+  }]
+
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.debug(info);
+    }
+  });
+}
