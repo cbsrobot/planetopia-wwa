0ac722b008cee572582c53d80a47c35243bd1238
Jean First <jeanfirst@gmail.com>
2022-10-08 13:31:22 +0200

    add WwaImage component

---
+++ src/components/WwaImage.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { _ } from "../modules/i18n.js";
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let textPath;
+</script>
+
+<div class="wwa">
+  <img src="assets/wwa.png" alt="" />
+  <p>{$_(textPath)}</p>
+</div>
+
+<style>
+  img {
+    position: absolute;
+    left: 100px;
+    bottom: -40px;
+  }
+
+  p {
+    font-size: 44px;
+    width: 100%;
+  }
+</style>
