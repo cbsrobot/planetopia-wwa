8f8c214e29952f15c30ae4a663ee05eb0995981c
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-10-31 09:24:57 +0100

    Update AreaSelectionPage

--- src/pages/AreaSelectionPage.svelte
+++ src/pages/AreaSelectionPage.svelte
@@ -1,7 +1,7 @@
 <script>
   import Navigation from "../components/Navigation.svelte";
   import Selectable from "../components/Selectable.svelte";
-  import { shuffleArray } from "../modules/PageUtils";
+  import Bubble from "../components/Bubble.svelte";
   import { userData, saveValue } from "../modules/DataManager";
   import { _ } from "../modules/i18n";
 
@@ -9,9 +9,8 @@
   export let pageIndex, totalPages;
 
   export let stationNumber, questionNumber;
-  export let selected = null;
+  let selected = null;
 
-  let overlayOpen = false;
   let answers = [
     { textKey: "answer0", points: 0 },
     { textKey: "answer1", points: 1 },
@@ -19,7 +18,6 @@
     { textKey: "answer3", points: 3 },
     { textKey: "special", points: 4 },
   ];
-  shuffleArray(answers);
 
   $: neutral = selected === null ? true : false;
 
@@ -40,75 +38,75 @@
 <Navigation bind:pageIndex={pageIndex} {totalPages} station={stationNumber} pageID={textPath} disableNext={neutral}/>
 
 <div class="content">
-  <img on:click={() => overlayOpen = true} alt="Insect Avatar" src="assets/avatar/{$userData.avatar}.jpg" />
-  <p class="question">{$_(textPath)}</p>
+  <div class="question-container">
+    <Bubble text={$_(textPath)}/>
+  </div>
+ 
   <div class="answer-container">
     <Selectable
       on:click={() => {
         selected = 0;
       }}
       selected={selected == 0}
-      text={$_(textPath, answers[0].textKey)}
+      text={$_("1")}
     />
     <Selectable
       on:click={() => {
         selected = 1;
       }}
       selected={selected == 1}
-      text={$_(textPath, answers[1].textKey)}
+      text={$_("2")}
     />
     <Selectable
       on:click={() => {
         selected = 2;
       }}
       selected={selected == 2}
-      text={$_(textPath, answers[2].textKey)}
+      text={$_("3")}
     />
     <Selectable
       on:click={() => {
         selected = 3;
       }}
       selected={selected == 3}
-      text={$_(textPath, answers[3].textKey)}
+      text={$_("4")}
     />
     <Selectable
       on:click={() => {
         selected = 4;
       }}
       selected={selected == 4}
-      text={$_(textPath, answers[4].textKey)}
+      text={$_(textPath, "special")}
     />
   </div>
 </div>
 
 <style>
-  img {
-    position: absolute;
-    top: 250px;
-    left: 0;
-    width: 400px;
-  }
 
   .content {
     /* position: absolute; */
     /* top: 20px; */
-    width: 70%;
+    width: 80%;
     height: 60%;
     display: flex;
-    flex-direction: column;
+    flex-direction:row;
     justify-content: flex-start;
     align-items: center;
   }
-  .question {
-    display: block;
-    text-align: left;
-    font-size: 52px;
-    width: 900px;
+
+  .question-container{
+    box-sizing: border-box;
+    padding-top: 30px;
+    height:100%;
+    width: 1000px;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
   }
 
   .answer-container {
     display: block;
-    width: 70%;
+    width: 500px;
   }
 
   p {
