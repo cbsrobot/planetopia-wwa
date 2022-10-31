0aecc4746ae8e1be24d510b9ed6cc9027bf3e4cd
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-10-31 09:25:56 +0100

    Update EmailAcceptPage

--- src/pages/EmailAcceptPage.svelte
+++ src/pages/EmailAcceptPage.svelte
@@ -1,107 +1,43 @@
 <script>
   import Navigation from "../components/Navigation.svelte";
-  import Selectable from "../components/Selectable.svelte";
-  import { shuffleArray } from "../modules/PageUtils";
-  import { userData, saveValue } from "../modules/DataManager";
+  import WwaImage from "../components/WwaImage.svelte";
   import { _ } from "../modules/i18n.js";
+  import { saveValue } from "../modules/DataManager";
+  import Bubble from "../components/Bubble.svelte";
+  import Button from "../components/Button.svelte";
 
   export let textPath;
   export let pageIndex, totalPages;
 
-  export let stationNumber, questionNumber;
-  export let selected = null;
-
-  let overlayOpen = false;
-  let answers = [
-    { textKey: "yes", points: 0 },
-    { textKey: "no", points: 1 },
-  ];
-  shuffleArray(answers);
-
-  $: neutral = selected === null ? true : false;
-
-  let points = null
-  // Convert points from database back to selected index
-  $: if (questionNumber in $userData.stations[stationNumber].questions) {
-      points = parseInt($userData.stations[stationNumber].questions[questionNumber])
-      const answerIndex = answers.findIndex((a) => a.points === points);
-      selected = (answerIndex < 0) ? null : answerIndex;
-    }
-
-  $: if (selected != null) {
-    saveValue(`stations.${stationNumber}.questions.${questionNumber}`, answers[selected].points)
-  }
-
+  export let stationNumber;
 </script>
 
-<Navigation bind:pageIndex={pageIndex} on:nextClicked {totalPages} station={stationNumber} pageID={textPath} disableNext={neutral}/>
-
-<img id="avatar" on:click={() => overlayOpen = true} alt="Insect Avatar" src="assets/avatar/{$userData.avatar}.jpg" />
-<img id="bubble" alt="bubble" src="assets/bubble_small.svg" />
-
-
+<Navigation bind:pageIndex nextHidden {totalPages} station={stationNumber} pageID={textPath}/>
+<WwaImage/>
 <div class="content">
-  <p class="question">{$_(textPath)}</p>
-  <div class="answer-container">
-    <Selectable
-      on:click={() => {
-        selected = 0;
-      }}
-      selected={selected == 0}
-      text={$_(answers[0].textKey, "")}
-    />
-    <Selectable
-      on:click={() => {
-        selected = 1;
-      }}
-      selected={selected == 1}
-      text={$_(answers[1].textKey, "")}
-    />
+  <div class="bubble-container">
+    <Bubble text={$_(textPath)} />
+  </div>
+  <div class="buttons-container">
+    <Button no on:click={() => {pageIndex += 2; saveValue(`wwa.emailAccepted`, false) }} handwritten={false} />
+    <Button yes on:click={() => {pageIndex += 1; saveValue(`wwa.emailAccepted`, true)}} handwritten={false} />
   </div>
 </div>
 
 <style>
-
-#avatar {
+  .content{
     position: absolute;
-    top: 250px;
-    left: 0;
-    width: 400px;
-  }
-  #bubble {
-    position: absolute;
-    top: 200px;
-    left: 320px;
-    width: 1060px;
+    width: 1000px;
+    top:350px;
+    left: 100px;
   }
 
-  .content {
-    /* position: absolute; */
-    /* top: 20px; */
-    width: 70%;
-    height: 60%;
+  .buttons-container{
+    margin-top: 30px;
+    box-sizing: border-box;
+    padding-right: 50px;
     display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    z-index: 2;
-  }
-  .question {
-    display: block;
-    text-align: left;
-    font-size: 52px;
-    width: 900px;
-  }
-
-  .answer-container {
-    position: absolute;
-    top: 560px;
-    left: 1120px;
-    display: block;
-    width: 20%;
-  }
-
-  p {
-    margin-top: 0;
+    justify-content: end;
+    width: 100%;
   }
 </style>
