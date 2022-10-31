77edddc16a32598d32f66f36ffbfadddc49e13b2
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-10-31 09:23:29 +0100

    Update WwaImage

--- src/components/WwaImage.svelte
+++ src/components/WwaImage.svelte
@@ -2,24 +2,55 @@
   import { _ } from "../modules/i18n.js";
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
-
-  export let textPath;
+  import { userData } from "../modules/DataManager";
 </script>
 
 <div class="wwa">
-  <img src="assets/wwa.png" alt="" />
-  <p>{$_(textPath)}</p>
+  <img class="background" src="assets/wwa-paper-texture.jpg" alt="" />
+  <img class="stamp" src="assets/wwa-stamp.jpg" alt="" />
+  <p>{$_($userData.wwa.textPath)}</p>
 </div>
 
 <style>
-  img {
+  .wwa {
     position: absolute;
-    left: 100px;
-    bottom: -40px;
+    width: 800px;
+    height: 550px;
+    right: 0px;
+    top: 300px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    box-shadow: 12px 15px 22px rgba(0, 0, 0, 0.25);
+  }
+  .background {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    object-fit: cover;
+    z-index: 1;
+  }
+
+  .stamp{
+    mix-blend-mode: multiply;
+    z-index: 2;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-top: 10px;
+    margin-bottom: -10px;
   }
 
   p {
-    font-size: 44px;
+    font-size: 36px;
     width: 100%;
+    box-sizing: border-box;
+    padding: 0 40px;
+    position: relative;
+    z-index: 2;
+    margin-top: 0;
   }
 </style>
