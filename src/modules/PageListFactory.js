4f912f4769f46487e2f412932d900a17f2b4169f
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-10-31 09:28:00 +0100

    Update PageListFactory

--- src/modules/PageListFactory.js
+++ src/modules/PageListFactory.js
@@ -6,6 +6,7 @@ import WwaConfirmationPage1 from "../pages/WwaConfirmationPage1.svelte";
 import WwaConfirmationPage2 from "../pages/WwaConfirmationPage2.svelte";
 import EmailAcceptPage from "../pages/EmailAcceptPage.svelte";
 import EmailPromptPage from "../pages/EmailPromptPage.svelte";
+import ResultPage from "../pages/ResultPage.svelte";
 import InfoPage from "../pages/InfoPage.svelte";
 import { userData } from "./DataManager.js";
 import { get } from "svelte/store";
@@ -98,14 +99,15 @@ const evaluationCore = [
   { component: DialoguePage, props: { textPath: "5.dialogue5" } },
   { component: DialoguePage, props: { textPath: "5.dialogue6" } },
   { component: DialoguePage, props: { textPath: "5.dialogue7" } },
+  { component: ResultPage, props: { textPath: "5.result" } },
   { component: AreaSelectionPage, props: { textPath: "5.areaSelection1" } }, // question_id 1
   { component: DialoguePage, props: { textPath: "5.dialogue8" } },
   { component: WwaSelectionPage, props: { textPath: "5.wwaSelection2" } }, // question_id 2
   { component: DialoguePage, depends: {"question_id":1, "options": ["5.dialogueClothes", "5.dialogueLiving", "5.dialogueMobility", "5.dialogueFood", "5.dialogueSpecial", "5.dialogueGeneral"]}, props: { textPath: "5" } },  
-  { component: WwaConfirmationPage1, props: { textPath: "5.wwaConfirmation3" }, conditionalJump:[null, 7] },
-  { component: WwaConfirmationPage2, props: { textPath: "5.wwaConfirmation4" }, conditionalJump:[null, 7] },
+  { component: WwaConfirmationPage1, props: { textPath: "5.wwaConfirmation3" } },
+  { component: WwaConfirmationPage2, props: { textPath: "5.wwaConfirmation4" } },
   { component: DialoguePage, props: { textPath: "5.dialogue9" } },
-  { component: EmailAcceptPage, props: { textPath: "5.emailAccept5" }, conditionalJump:[null, 16] },
+  { component: EmailAcceptPage, props: { textPath: "5.emailAccept5" } },
   { component: EmailPromptPage, props: { textPath: "5.emailPrompt6" } },
   { component: DialoguePage, props: { textPath: "5.dialogue10", markComplete: true, stationCompleted: 5 } },
 ];
