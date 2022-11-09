9830e658ef9fb72c3d86d4853912ed6abb547e57
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-11-09 10:08:25 +0100

    Update InfoOverlay with insect names

--- src/components/InfoOverlay.svelte
+++ src/components/InfoOverlay.svelte
@@ -2,8 +2,16 @@
   import { _ } from "../modules/i18n.js";
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
+  import { userData } from "../modules/DataManager";
+  // import { userData } from "../modules/PageUtils.js";
 
   export let textPath;
+  export let showInsectNames = true;
+
+  $: avatarNr = $userData.avatar;
+  $: name = $_(`insect${avatarNr}.name`);
+  $: scientificName = $_(`insect${avatarNr}.scientificName`);
+  $: showScientificName = !name.includes(scientificName);
 </script>
 
 <div class="background" />
@@ -11,7 +19,15 @@
   <button on:click={() => dispatch("exit")}>
     <img src="assets/icons/cancel.svg" alt="" />
   </button>
-  <p>{$_(textPath)}</p>
+  <div class="text-wrapper">
+    <p>{$_(textPath)}</p>
+  </div>
+  {#if showInsectNames}
+    <span class="name">{name}</span>
+    {#if showScientificName}
+      <span class="scientific-name">{scientificName}</span>
+    {/if}
+  {/if}
 </div>
 
 <style>
@@ -20,7 +36,7 @@
     opacity: 0.6;
     width: 100%;
     height: 100%;
-    
+
     position: absolute;
     top: 0;
     left: 0;
@@ -35,14 +51,15 @@
     transform: translate(-50%, -50%);
     background-color: white;
     width: 50%;
-    height: 40%;
+    min-height: 40%;
     padding: 10rem;
-    text-align: center;
-    padding: 4rem;
+    text-align: start;
+    padding: 3rem 4rem 2.6rem 3rem;
 
     display: flex;
-    justify-content: center;
-    align-items: center;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: flex-start;
 
     border: 8px solid #2d2d2d;
     box-shadow: 18px 17px 53px rgba(0, 0, 0, 0.25);
@@ -62,7 +79,7 @@
 
     display: block;
     align-self: center;
-    padding: 1rem 1rem; 
+    padding: 1rem 1rem;
     margin: 0;
     color: #2d2d2d;
     font-size: 2rem;
@@ -76,8 +93,23 @@
     background-color: transparent;
   }
 
+  .text-wrapper {
+    flex-grow: 1;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
   p {
     font-size: 44px;
     width: 100%;
+  }
+
+  .name {
+    font-size: 34px;
+  }
+
+  .scientific-name {
+    font-size: 22px;
   }
 </style>
