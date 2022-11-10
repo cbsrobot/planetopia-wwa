f6af75e39ced7d07beba2c55c5176ccb7a643746
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-11-10 21:18:05 +0100

    WwaImage: Add wwa number string

--- src/components/WwaImage.svelte
+++ src/components/WwaImage.svelte
@@ -1,12 +1,13 @@
 <script>
   import { _ } from "../modules/i18n.js";
-  import { createEventDispatcher } from "svelte";
-  const dispatch = createEventDispatcher();
   import { userData } from "../modules/DataManager";
   // import { userData } from "../modules/PageUtils.js";
+  import { fly } from 'svelte/transition';
 
   export let attested = false;
-  import { fly } from 'svelte/transition';
+
+  let wwaNumberStr = "D - 000001" // TODO: Get Number from userData
+  
 </script>
 
 <div class="wwa">
@@ -14,21 +15,26 @@
     <img in:fly|local={{duration: 2000}} class="attested-stamp" src="assets/attested-stamp.png" alt="" />
   {/if}
   <img class="background" src="assets/wwa-paper-texture.jpg" alt="" />
-  <img class="stamp" src="assets/wwa-stamp.jpg" alt="" />
+  <div class="wwa-stamp-wrapper">
+    <img class="wwa-stamp" src="assets/wwa-stamp.jpg" alt="" />
+    <span class="wwa-number">{wwaNumberStr}</span>
+  </div>
   <p>{$_($userData?.wwa.textPath)}</p> 
 </div>
 
 <style>
   .wwa {
     position: absolute;
-    width: 800px;
-    height: 550px;
+    top: 50%;
     right: 0px;
-    top: 300px;
+    transform: translateY(-50%);
+
+    width: 800px;
 
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;
 
     box-shadow: 12px 15px 22px rgba(0, 0, 0, 0.25);
   }
@@ -42,13 +48,28 @@
     z-index: 1;
   }
 
-  .stamp{
-    mix-blend-mode: multiply;
+  .wwa-stamp-wrapper{
     z-index: 2;
-    margin-left: 10px;
-    margin-right: 10px;
+   
+    width: 98%;
     margin-top: 10px;
     margin-bottom: -10px;
+    mix-blend-mode: multiply;
+    position: relative;
+  }
+
+  .wwa-stamp{
+    box-sizing: border-box;
+    width: 100%
+  }
+
+  .wwa-number{
+    position: absolute;
+    bottom: 45px;
+    left: 0;
+    width: 100%;
+    font-size: 28px;
+    text-transform: uppercase;
   }
 
   .attested-stamp{
@@ -68,5 +89,6 @@
     position: relative;
     z-index: 2;
     margin-top: 0;
+    margin-bottom: 40px;
   }
 </style>
