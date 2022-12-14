48b44723df30e1ae358e1fa29441cd6269562719
Jonas Wolter <jonas.wolter.ch@gmail.com>
2022-12-14 17:15:30 +0100

    Selectable: Add custom wwa option

--- src/components/Selectable.svelte
+++ src/components/Selectable.svelte
@@ -1,13 +1,26 @@
 <script>
+  import { _ } from "../modules/i18n.js";
+
   export let selected = false;
   export let text = "Some text";
 
-  $: neutral = selected != true
+  export let customWwa = false;
+  $: if (customWwa) {
+    text = $_("5.wwaSelection2.createCustom")
+  }
+
+  $: neutral = selected != true;
 </script>
 
-<button on:click class:selected class:neutral>
-  <div>{text}</div>
-  <img alt="" src="assets/icons/tick.svg" class:hidden = {!selected}/>
+<button on:click class:selected class:neutral class:customWwa>
+  <div>
+    {#if customWwa}
+      <img class="pencil" alt="" src="assets/icons/pencil.svg" />
+    {/if}
+    {text}
+  </div>
+  <img class="tick" alt="" src="assets/icons/tick.svg" class:hidden={!selected}
+  />
 </button>
 
 <style>
@@ -16,7 +29,7 @@
     align-items: center;
     justify-content: space-between;
 
-    align-self:stretch;
+    align-self: stretch;
     padding: 1rem 1rem;
     margin: 1rem 1rem;
     color: #2d2d2d;
@@ -32,15 +45,21 @@
     border-bottom-right-radius: 225px 15px;
     border-bottom-left-radius: 15px 255px;
 
-    width:100%;
+    width: 100%;
   }
 
-  .selected{
+  .customWwa {
+    border-style: dashed;
+    /* border: 5px dashed #000000; */
+  }
+
+  .selected {
     box-shadow: 0 0 0 3px #2d2d2d;
+    border-style: solid;
     background-color: white;
   }
 
-  .neutral{
+  .neutral {
     background-color: #f5f5f5;
   }
 
@@ -49,20 +68,27 @@
     display: inline;
     overflow-wrap: break-word;
     text-align: start;
-  } 
+  }
 
-  img {
+  .tick {
     padding: 1rem 0.5rem;
     height: 2.2rem;
     width: 2.2rem;
   }
 
-  button:active {
-    background-color: white;
-  } 
-
-  .hidden{
-    visibility: hidden;
+  .pencil{
+    padding: 0 2.8rem;
+    position: relative;
+    top: 0.22rem;
+    left: 0;
+    /* width: 2.2rem; */
   }
 
+  button:active {
+    background-color: white;
+  }
+
+  .hidden {
+    visibility: hidden;
+  }
 </style>
